c0b242a927904cae58f8802d84bf2be214812f1a
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2024-10-30 22:34:19 +0900

    add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,24 @@
+import { Noto_Sans_JP, Roboto_Mono } from 'next/font/google'
+import { twMerge } from 'tailwind-merge'
 import './globals.css'
 import { Providers } from './providers'
 
+const NotoSansJp = Noto_Sans_JP({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-noto-sans-jp',
+})
+
+const RobotoMono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto-mono',
+})
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='ja'>
-      <body>
+    <html lang='ja' className={`${NotoSansJp.variable} ${RobotoMono.variable}`} suppressHydrationWarning>
+      <body className={twMerge('font-noto bg-background antialiased')}>
         <Providers>{children}</Providers>
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,27 @@ const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        noto: [
+          'var(--font-noto-sans-jp)',
+          'BlinkMacSystemFont',
+          'Roboto',
+          'Segoe UI',
+          'Helvetica Neue',
+          'HelveticaNeue',
+          '游ゴシック体',
+          'YuGothic',
+          '游ゴシック Medium',
+          'Yu Gothic Medium',
+          '游ゴシック',
+          'Yu Gothic',
+          'Verdana',
+          'メイリオ',
+          'Meiryo',
+          'sans-serif',
+        ],
+        mono: ['var(--font-roboto-mono)'],
+      },
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
