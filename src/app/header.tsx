337749f830834c1a6335b517e53f0560ea11866b
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2024-11-01 21:50:04 +0900

    update Header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,12 +1,21 @@
 'use client'
 
 import { ThemeSwitchList } from '@/components/theme-sw'
+import { Button } from '@nextui-org/react'
 import { FC } from 'react'
 
 export const Header: FC = () => {
   return (
-    <nav className='sticky top-0'>
-      <ThemeSwitchList size='sm' variant='light' />
+    <nav className='sticky top-0 flex bg-gray-100 p-1 dark:bg-gray-900'>
+      <div className='flex-initial'>
+        <Button size='sm' variant='light'>
+          Open
+        </Button>
+      </div>
+      <div className='flex-auto'></div>
+      <div className='flex-initial'>
+        <ThemeSwitchList size='sm' variant='light' />
+      </div>
     </nav>
   )
 }
