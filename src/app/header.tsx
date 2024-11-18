bba8c800e50c36f0d5687c04a2695b5e5350c552
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2024-11-18 22:57:34 +0900

    ファイル監視機能を追加

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ThemeSwitchList } from '@/components/theme-sw'
-import { Button } from '@nextui-org/react'
+import { Button, Switch } from '@nextui-org/react'
 import { open } from '@tauri-apps/plugin-dialog'
 import { BaseDirectory, readTextFile, watch } from '@tauri-apps/plugin-fs'
 import { FC, useEffect, useState } from 'react'
@@ -10,6 +10,8 @@ import { useSharedUIContext } from './context'
 export const Header: FC = () => {
   const { setMdContents } = useSharedUIContext()
   const [filePath, setFilePath] = useState<string>()
+  const [isWatch, setWatch] = useState(false)
+  const [unWatch, setUnWatch] = useState<ReturnType<typeof watch>>()
 
   useEffect(() => {
     if (filePath) {
@@ -19,22 +21,33 @@ export const Header: FC = () => {
   }, [filePath])
 
   useEffect(() => {
-    if (filePath) {
-      watch(
-        filePath,
-        () => {
-          if (filePath) {
-            readTextFile(filePath, { baseDir: BaseDirectory.AppConfig }).then((contents) => setMdContents(contents))
-          }
-        },
-        {
-          baseDir: BaseDirectory.AppConfig,
-          delayMs: 1000,
-        },
+    if (filePath && isWatch) {
+      setUnWatch(
+        watch(
+          filePath,
+          () => {
+            if (filePath) {
+              readTextFile(filePath, { baseDir: BaseDirectory.AppConfig }).then((contents) => setMdContents(contents))
+            }
+          },
+          {
+            baseDir: BaseDirectory.AppConfig,
+            delayMs: 1000,
+          },
+        ),
       )
+      console.debug('watch on')
+    } else {
+      if (unWatch) {
+        unWatch.then((fn) => {
+          fn()
+          setUnWatch(undefined)
+          console.debug('watch off')
+        })
+      }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filePath])
+  }, [filePath, isWatch])
 
   const openFile = async () => {
     const selected = await open({
@@ -61,10 +74,13 @@ export const Header: FC = () => {
 
   return (
     <nav className='sticky top-0 flex bg-gray-100 p-1 dark:bg-gray-900'>
-      <div className='flex-initial'>
+      <div className='flex flex-initial'>
         <Button size='sm' variant='light' onPress={openFile}>
           Open
         </Button>
+        <Switch className='ml-2 items-center' size='sm' isSelected={isWatch} onValueChange={setWatch}>
+          Watch
+        </Switch>
       </div>
       <div className='flex-auto'></div>
       <div className='flex-initial'>
