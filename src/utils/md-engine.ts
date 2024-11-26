256347be0632553712aa74dd2842c84e8c2eaaa4
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2024-11-26 21:54:44 +0900

    add rehype-slug

--- src/utils/md-engine.ts
+++ src/utils/md-engine.ts
@@ -1,5 +1,6 @@
 import rehypeCodeTitles from 'rehype-code-titles'
 import rehypePrism from 'rehype-prism-plus'
+import rehypeSlug from 'rehype-slug'
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
@@ -11,6 +12,7 @@ export const md2Html = async (md: string) => {
     .use(remarkRehype)
     .use(rehypeCodeTitles)
     .use(rehypePrism)
+    .use(rehypeSlug)
     .use(rehypeStringify)
     .process(md)
   return res.toString()
