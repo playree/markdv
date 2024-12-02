dd0548a403e1c3092a15d34bcb635860ba5a2357
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2024-12-02 22:37:45 +0900

    エディタを組み込み中

--- src/app/edit/view.tsx
+++ src/app/edit/view.tsx
@@ -1,8 +1,43 @@
 'use client'
 
-import { FC } from 'react'
+import 'easymde/dist/easymde.min.css'
+import { FC, useCallback, useMemo, useState } from 'react'
+import { SimpleMdeReact, SimpleMDEReactProps } from 'react-simplemde-editor'
+import './custom.css'
 
 export const View: FC = () => {
-  return <div>edit</div>
+  const [value, setValue] = useState('Initial value')
+
+  const options = useMemo(() => {
+    return {
+      autofocus: true,
+      spellChecker: false,
+      toolbar: [
+        'heading',
+        'bold',
+        'italic',
+        'strikethrough',
+        'quote',
+        '|',
+        'code',
+        'link',
+        'horizontal-rule',
+        '|',
+        'unordered-list',
+        'ordered-list',
+        'table',
+      ],
+    } as SimpleMDEReactProps['options']
+  }, [])
+
+  const onChange = useCallback((value: string) => {
+    setValue(value)
+  }, [])
+
+  return (
+    <>
+      <SimpleMdeReact value={value} onChange={onChange} options={options} />
+    </>
+  )
 }
 View.displayName = 'View'
